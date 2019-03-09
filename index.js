b2ca3634da6999ba0e1c47186e05f6dda487aeaf
Higor Diego <higordiegoti@gmail.com>
2019-03-09 17:05:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var socket = require('socket.io-client')('http://34.219.164.43:3500');
 socket.on('connect', function () {
     console.log('connectado')
     socket.on('message', function (msg) {
-        socket.emit('post', JSON.stringify({ rooms: 'porradeirasinistra', text: 'bot03' }));
+        console.log('msg', msg)
+        socket.emit('post', JSON.stringify({ rooms: 'porradeirasinistra', text: 'bot03' }))
     })
 })
